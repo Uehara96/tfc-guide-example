56a13034c4ff887b7c9aa4f4134fb2dd640d46a6
Milena Suemi Uehara <42937807+Uehara96@users.noreply.github.com>
2020-08-06 21:43:11 -0300

    Update main2.tf

--- main2.tf
+++ main2.tf
@@ -4,11 +4,11 @@ provider "aws" {
   region = var.aws_west
 }
 
-resource "aws_instance" "provisioners" {
+resource "aws_instance" "example" {
   ami           = "ami-b374d5a5"
   instance_type = "t2.micro"
 
   provisioner "local-exec" {
-    command = "echo ${aws_instance.provisioners.public_ip} > ip_address.txt"
+    command = "echo ${aws_instance.example.public_ip} > ip_address.txt"
   }
 }
